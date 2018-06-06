8c6ad854f2d47335b4f335758631492b1ffbdf79
Aniruddh Marellapudi <amarellapudi@gmail.com>
2018-06-06 11:31:11 -0400

    Exporting
    adding exporting functionality to addin

---
+++ solidworks-api-develop/Tutorials/Exporting/Exporting/Exporting/SolidDnaIntegration.cs
@@ -0,0 +1,55 @@
+﻿using AngelSix.SolidDna;
+using Dna;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exporting
+{   
+    /// <summary>
+    /// Register as a SolidWorks Add-In
+    /// </summary>
+    public class SolidDnaAddinIntegration : AddInIntegration
+    {
+        public override void ApplicationStartup()
+        {
+        }
+
+        public override void ConfigureServices(FrameworkConstruction construction)
+        {
+            
+        }
+
+        public override void PreConnectToSolidWorks()
+        {
+            
+        }
+
+        public override void PreLoadPlugIns()
+        {
+            
+        }
+    }
+
+    /// <summary>
+    /// Register as SolidDna Plugin
+    /// </summary>
+    public class MySolidDnaPlugIn : SolidPlugIn
+    {
+        public override string AddInTitle => "Exporting Functionality";
+
+        public override string AddInDescription => "Hoping to export .STL";
+
+        public override void ConnectedToSolidWorks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DisconnectedFromSolidWorks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
