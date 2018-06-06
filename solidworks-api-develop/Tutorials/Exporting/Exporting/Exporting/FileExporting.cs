214c8eacfb5a003a094dce9ef9bad42c79bccbf1
Aniruddh Marellapudi <amarellapudi@gmail.com>
2018-06-06 13:22:34 -0400

    Exporting
    added STEP file export capability

--- solidworks-api-develop/Tutorials/Exporting/Exporting/Exporting/FileExporting.cs
+++ solidworks-api-develop/Tutorials/Exporting/Exporting/Exporting/FileExporting.cs
@@ -39,13 +39,35 @@ namespace Exporting
                 // Tell user success
                 Application.ShowMessageBox("Successfully saved part as DXF");
             else
+                // Tell user failed
                 Application.ShowMessageBox("Failed to save part as DXF", SolidWorksMessageBoxIcon.Stop);
         }
 
 
         public static void ExportModelAsStep()
         {
+            // Make Sure it's a part or assembly
+            if (Application.ActiveModel?.IsPart != true && Application.ActiveModel?.IsAssembly != true)
+            {
+                // Tell user
+                Application.ShowMessageBox("Active model is not a part or assembly", SolidWorksMessageBoxIcon.Stop);
 
+                return;
+            }
+
+            // Ask user for location
+            var location = GetSaveLocation("STEP File|*.step", "Save Model as STEP");
+
+            // Check if user clicked cancel
+            if (string.IsNullOrEmpty(location))
+                return;
+
+            if (!SaveModelAs(location))
+                // Tell user failed
+                Application.ShowMessageBox("Failed to save model as STEP", SolidWorksMessageBoxIcon.Stop);
+            else
+                // Tell user success
+                Application.ShowMessageBox("Successfully saved model as STEP");
         }
 
         public static void ExportDrawingAsPdf()
@@ -55,6 +77,31 @@ namespace Exporting
         #endregion
 
         #region Private Helpers
+
+        /// <summary>
+        /// Attempts to save a model as a different format
+        /// </summary>
+        /// <param name="location">The absolute path to save the model</param>
+        /// <param name="exportData">The export data, if any</param>
+        /// <returns></returns>
+        private static bool SaveModelAs(string location, object exportData = null)
+        {
+            var error = -1;
+            var warning = -1;
+
+            var version = (int)swSaveAsVersion_e.swSaveAsCurrentVersion;
+            var options = (int)(swSaveAsOptions_e.swSaveAsOptions_Copy | swSaveAsOptions_e.swSaveAsOptions_Silent | swSaveAsOptions_e.swSaveAsOptions_UpdateInactiveViews);
+
+            // Save model as...
+            Application.ActiveModel.Extension.UnsafeObject.SaveAs(location, version, options, exportData, ref error, ref warning);
+
+            // If this fails, try one other wa
+            if (error != 0)
+                Application.ActiveModel.UnsafeObject.SaveAs4(location, version, options, ref error, ref warning);
+
+            // Return success result
+            return error == 0;
+        }
 
         /// <summary>
         /// Asks the user for a save location of a file
