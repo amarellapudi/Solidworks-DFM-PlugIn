fdb1098f9446df5c009e228cf3c4e551b2e31738
Aniruddh Marellapudi <amarellapudi@gmail.com>
2018-07-06 10:01:40 -0400

    week 5/6 update ppt

--- SculptPrint/Parts/i.py
+++
@@ -1,7 +0,0 @@
-import sculptprint
-
-sculptprint.start()
-sculptprint.new()
-sculptprint.import_part(r'C:\Users\Aniruddh\OneDrive - Georgia Institute of Technology\CASS\Solidworks-DFM-PlugIn\SculptPrint\Parts\test.STL')
-sculptprint.export_animated_view(r'', r'export', r'png', 30.00000, -15.23292, -136.25829, -19.43503, 0.00000, 0.00000, 0.00000, 0.04277, -0.14576, 0.98840, 1)
-sculptprint.stop()
