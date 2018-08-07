7522804cae8f039db0af632ea30191de93d1fe29
Aniruddh Marellapudi <amarellapudi@gmail.com>
2018-08-07 16:10:34 -0400

    week 10 cleanups

---
+++ Code/Prototypes/SongTelenkoDFM/MessageBox_DFMLoading.cs
@@ -0,0 +1,59 @@
+﻿using Renci.SshNet;
+using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SongTelenkoDFM
+{
+    public partial class MessageBox_DFMLoading : Form
+    {
+        public string FileName { get; set; }
+        public SftpClient Client { get; set; }
+
+        public MessageBox_DFMLoading(string fileName, SftpClient client)
+        {
+            InitializeComponent();
+            FileName = fileName;
+            Client = client;
+
+            UI_SolidWorks_SideBar_PlugIn.SFTPUploadFile(client, "View_SW.png");
+            UI_SolidWorks_SideBar_PlugIn.SFTPUploadFile(client, "test.stl");
+            UI_SolidWorks_SideBar_PlugIn.CreateFinishedFlag();
+        }
+
+        // Disable close button
+        private const int CP_NOCLOSE_BUTTON = 0x200;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams myCp = base.CreateParams;
+                myCp.ClassStyle = myCp.ClassStyle | CP_NOCLOSE_BUTTON;
+                return myCp;
+            }
+        }
+
+        public void Window_ContentRendered(object sender, EventArgs e)
+        {
+            var worker = new BackgroundWorker();
+
+            worker.DoWork += Worker_DoWork;
+
+            worker.RunWorkerAsync();
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (!UI_SolidWorks_SideBar_PlugIn.DownloadFile(Client, "DONE_researcher"))
+            {
+                Thread.Sleep(75);
+            }
+            UI_SolidWorks_SideBar_PlugIn.DownloadFile(Client, FileName);
+            Client.DeleteFile("DONE_researcher");
+            Client.DeleteFile("View_Researcher_Feedback.png");
+
+            DialogResult = DialogResult.Yes;
+        }
+    }
+}
---
+++ Code/Prototypes/SongTelenkoDFM/MessageBox_DFMResults.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace SongTelenkoDFM
+{
+    partial class MessageBox_DFMResults
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.ImageLocation = "";
+            this.pictureBox.Location = new System.Drawing.Point(20, 20);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(926, 433);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // MessageBox_DFMResults
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(966, 473);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "MessageBox_DFMResults";
+            this.Padding = new System.Windows.Forms.Padding(20);
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "SculptPrint DFM Result";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
